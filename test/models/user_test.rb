12e551cd1d59f6d8ff4f0920f7db4c077987af23
alex174 <axchang777@gmail.com>
2021-11-14 19:37:04 -0800

    added new tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new user creation" do
+    user = User.new
+    assert true
+  end
+  test "proper email" do
+    user = User.new
+    user.email = "test@gmail.com"
+    assert true
+  end
+  test "deny improper emails" do
+    user = User.new
+    user.email = "notanemail"
+    assert false
+  end
+  test "deny bad passwords" do
+    user = User.new
+    user.password = "notagoodpass"
+    assert false
+  end
+  test "allow good password" do
+    user = User.new
+    user.password = "goodpass123!"
+    assert true
+  end
 end
