87321ad9418fc3b165c968379f2a3728469b85d1
Alex Chang <axchang777@gmail.com>
2021-11-28 15:14:22 -0800

    schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_204203) do
+ActiveRecord::Schema.define(version: 2021_11_25_032950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 2021_11_19_204203) do
   create_table "followers", force: :cascade do |t|
     t.string "from"
     t.string "to"
-    
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "forum_posts", force: :cascade do |t|
     t.integer "forum_thread_id"
     t.integer "user_id"
