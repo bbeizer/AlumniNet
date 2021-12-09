0e5bc91f91b48c3a563d783ca5132401934863f1
Alex Chang <axchang777@gmail.com>
2021-12-08 19:58:25 -0800

    added more tests

--- test/controllers/forum_threads_controller_test.rb
+++ test/controllers/forum_threads_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ForumThreadsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create ForumThreads entry" do
+    assert_difference('ForumThreads'.count) do
+      ForumThreads :create, ForumThreads: {from: '1', to: '2'}
+    end
+    assert_redirected_to_ForumThreads(assigns(:ForumThreads))
+    assert_equal 'ForumThreads entry was successfully created.', flash[:notice]
+  end
 end
