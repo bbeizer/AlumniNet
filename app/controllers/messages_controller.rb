8a139e7bd78f3099ee2bebdba876a39ec0f4d014
Benjamin Beizer <bhb987@gmail.com>
2021-12-13 21:42:28 -0500

    fixed conversations so that the text box would get displayed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,11 @@ class MessagesController < ApplicationController
     def create
       receipt = current_user.reply_to_conversation(@conversation, params[:body])
       redirect_to conversation_path(receipt.conversation)
-      params.require(:message).permit(:title, :content)
-      redirect_to message
     end
     
     private
 
     def set_conversation
-      @conversation = current_user.mailbox.conversation.find(params[:conversation_id])
+      @conversation = current_user.mailbox.conversations.find(params[:conversation_id])
     end
 end
